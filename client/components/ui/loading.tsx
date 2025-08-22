3c2d0dd53f6375475483b1c86e87c4c029a5a845
Bryan Ríos <bryanriosb01@gmail.com>
2025-08-22 04:21:04 -0500

    delete client dir

--- client/components/ui/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from 'lucide-react'
-
-export default function Loading({
-  className = 'w-4 h-4',
-}: {
-  className?: string
-}) {
-  return <Loader2 className={`${className} animate-spin`} />
-}
--- client/lib/actions/getenv.ts
+++
@@ -1,6 +0,0 @@
-'use server'
-
-export async function getEnv(env: string) {
-  const value = process.env[env]
-  return value
-}
